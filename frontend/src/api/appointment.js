25f4d6e6b7a0302eedd2162e74187603b13f3864
Vegimi-13 <vegimahmetaj@gmail.com>
2026-01-30 04:34:04 +0100

    added axios for frontend for appointments

---
+++ frontend/src/api/appointment.js
@@ -0,0 +1,9 @@
+import api from "./axios"
+
+export const createAppointment = (data) => {
+  return api.post("/appointments", data)
+}
+
+export const getMyAppointments = () => {
+  return api.get("/appointments/me")
+}
